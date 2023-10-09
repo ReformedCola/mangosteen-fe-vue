da0f4f214225fab73cd290534247f96695268464
Jason He <zhiyuanfw@gmail.com>
2023-10-10 06:27:54 +0800

    rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/mangosteen-vue/',
+  base: '/mangosteen-fe-vue/dist/',
   plugins: [vue()]
 })
