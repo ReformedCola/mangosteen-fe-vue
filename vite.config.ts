ef2598f034a3835fd2efe3030f25a644632966b6
Jason He <zhiyuanfw@gmail.com>
2023-10-10 06:32:30 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen-fe-vue/dist/',
+  // base: '/mangosteen-fe-vue/dist/',
   plugins: [vue()]
 })
