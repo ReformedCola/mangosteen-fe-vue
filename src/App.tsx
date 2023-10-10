d8ec5a6b50e619eca94ffeca45ced2e4eda67797
Jason He <zhiyuanfw@gmail.com>
2023-10-10 14:15:32 +0800

    feature: created welcome pages and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import "./App.scss"
 
 export const App = defineComponent({
   setup() {
-    return () => <>
-      <header>Nav
-        <ul>
-          <li>
-            <router-link to="/">Foo</router-link>
-          </li>
-          <li>
-            <router-link to="/bar">Bar</router-link>
-          </li>
-        </ul>
-      </header>
+    return () => (
       <div>
         <RouterView/>
       </div>
-      <footer>Footer</footer>
-    </>
+    )
   }
 })
---
+++ src/views/Welcome.tsx
@@ -0,0 +1,15 @@
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+import s from './Welcome.module.scss'
+import logo from '../assets/icons/mangosteen.svg'
+export const Welcome = defineComponent({
+  setup: (props, context) => {
+    return () => <div class={s.wrapper}>
+      <header>
+        <img src={logo} alt="mangosteen" />
+        <h1>Mangosteen</h1>
+      </header>
+      <main><RouterView/></main>
+    </div>
+  }
+})
