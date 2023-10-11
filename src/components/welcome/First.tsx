c5000bcd2610da41783d501ccbbb7f235de3af5f
Jason He <zhiyuanfw@gmail.com>
2023-10-12 06:59:52 +0800

    feature: finish four welcome pages

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -7,8 +7,8 @@ export const First = defineComponent({
     return () => (
       <div class={s.wrapper}>
         <div class={s.card}>
-          <img class={s.pig} src={pig} />
-          <h2>Earn Smart<br/>Save Smart</h2>
+          <img src={pig} />
+          <h2>Earn Smart<br />Save Smart</h2>
         </div>
         <div class={s.actions}>
           <RouterLink class={s.fake} to="/welcome/1">Skip</RouterLink>
--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,8 +1,21 @@
 import { defineComponent } from 'vue';
+import s from './First.module.scss';
+import cloud from '../../assets/icons/cloud.svg';
+import { RouterLink } from 'vue-router';
 export const Forth = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>Forth</div>
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={cloud} />
+          <h2>Cloud Backup<br />Never Lose Data</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/welcome/1">Skip</RouterLink>
+          <RouterLink to="/welcome/1">Finish</RouterLink>
+          <RouterLink class={s.fake} to="/welcome/1">Skip</RouterLink>
+        </div>
+      </div>
     )
   }
 })
